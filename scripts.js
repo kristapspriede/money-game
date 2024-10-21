3899dbb314a3df2c91eada14f8a4e8bcc3f19b80
kristapspriede <kristaps.priede@gmail.com>
2024-10-21 21:37:30 +0300

    Updated html, added trainings

--- scripts.js
+++ scripts.js
@@ -108,9 +108,21 @@ const janitorJob = new Job("Janitor", 100);
 const jobs = [ janitorJob ];
 
 // Trainings
-const basicTraining = new Training("Basic Skills", 50, 1)
+const basicTraining = new Training("Basic Skills", 50, 1);
+const bartendingTraining = new Training("Customer Service Course", 100, 2);
+const waiterTraining = new Training("Hospitality Course", 150, 3);
+const teachingTraining = new Training("Teaching License", 300, 6);
+const carpentryTraining = new Training("Carpentry Certification", 500, 5);
+const mechanicsTraining = new Training("Auto Repair Course", 600, 4);
+const electricianTraining = new Training("Electrical Engineering Certification", 700, 5);
+const codingTraining = new Training("Coding Bootcamp", 1000, 10);
+const nursingTraining = new Training("Nursing Degree", 1200, 8);
+const constructionTraining = new Training("Construction Management Degree", 1500, 7);
+const marketingTraining = new Training("Business Degree", 2000, 12);
+const lawTraining = new Training("Law School Degree", 3000, 24);
+const medicalTraining = new Training("Medical School Degree", 5000, 36);
 
-const trainings = [ basicTraining ];
+const trainings = [ basicTraining, bartendingTraining, waiterTraining, teachingTraining, carpentryTraining, mechanicsTraining, electricianTraining, codingTraining, nursingTraining, constructionTraining, marketingTraining, lawTraining, medicalTraining ];
 
 // utilities
 
